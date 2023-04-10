1edccb877d1b7dba9ba233fbd9832e2a4719b89e
vikas-bareth <iamvikasbareth@gmail.com>
2023-04-11 00:41:03 +0530

    complete console game for rock paper scissors

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
-const choices = ['Rock','Paper','Scissors'];
+const choices = ['rock','paper','scissors'];
+
+function getPlayerChoice(){
+    const choice = prompt('enter your choice rock, paper or scissors').toLowerCase();
+    return choice;
+}
 
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
     const computerChoice = choices[randomNumber];
     return computerChoice;
 }
+
+function playRound(playerSelection,computerSelection){
+    //check for a tie
+    if(playerSelection === computerSelection){
+        return 'tie';
+    }
+    //check for playerWin condition
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection === 'paper' && computerSelection === 'rock') || 
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ){
+        return 'win';
+    }
+    //check for computer win
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'paper') || 
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock')
+    ) {
+        return 'lose';
+    }
+
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0 ; i < 5; i++){
+        console.log(`Round:${i+1}`)
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection,computerSelection);
+        if(result === 'win'){
+            playerScore++;
+            console.log(`You win ${playerSelection} beats ${computerSelection}`)
+            console.log(`Your-Score:${playerScore} Computer-Score:${computerScore}`)
+        } else if (result === 'lose'){
+            computerScore++
+            console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+            console.log(`Your-Score:${playerScore} Computer-Score:${computerScore}`)
+            ;
+        } else {
+            console.log(`It's a tie you both choose ${playerSelection}`)
+            console.log(`Your-Score:${playerScore} Computer-Score:${computerScore}`)
+        }
+    }
+    console.log("*****************************")
+    if(playerScore > computerScore){
+        console.log("Congratulation you won with a total Score of:")
+        console.log(`Your-final-Score:${playerScore},Computer-final-Score:${computerScore}`)
+    } else {
+        console.log("Oops! Bad luck you lose")
+        console.log(`Your-final-Score:${playerScore},Computer-final-Score:${computerScore}`)
+    }
+}
+game();
